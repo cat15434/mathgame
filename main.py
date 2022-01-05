768461acbc3eff68b4facc436e3dc30fb8b49f82
user <cat15434@gmail.com>
2022-01-05 12:39:40 -0500

    end my suffering please...

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
-
 import random
 import tkinter as tk
+
+import time as ti
+
 window=tk.Tk()
 
 
+entry=tk.Entry(window)
+def play():
+    button2 = tk.Button(command=play())
+    button3=button2
+    a= random.randint(0,101)
+    b= random.randint(0,101)
+    c=a*b
+    z=str(c)
+    value2 = tk.Label(text=a)
+    value1 = tk.Label(text=b)
+    value3 = tk.Label(text=c)
+    value5 = tk.Label(text="Correct")
+    value6 = tk.Label(text = "Game Over")
+    value7 = tk.Label(text = "mmmm beans")
+    print(c)
+    def hi() :
+        print(entry.get())
+        f=entry.get()
+        if 'beans' in f :
+            print("mmmm beans")
+            value7.pack()
+            window.after(2000,value7.destroy)
+        if f == z:
+            f = int(entry.get())
+            print ("correct")
+            value5.pack()
+            window.after(2000, value5.destroy)
 
-a= random.randint(0,101)
-b= random.randint(0,101)
-d=int(f.get())
-c=a*b
-value2 = tk.Label(text=a)
-value1 = tk.Label(text=b)
-value3 = tk.Label(text=c)
+        elif f != z:
+            f = int(entry.get())
+            print("incorrect")
+            value6.pack()
+            window.after(2000, value6.destroy)
 
 
-f = tk.Entry()
-f.pack()
-value1.pack()
-value2.pack()
-value3.pack()
+    button=tk.Button(command=play())
 
-
+    entry.bind("<Return>",hi)
+    value1.pack()
+    value2.pack()
+    value3.pack()
+    entry.pack()
+    button.pack()
 
 window.mainloop()
