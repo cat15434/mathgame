a1725b21ec8ce2d1a5346f9852426ec301a4f38a
user <cat15434@gmail.com>
2022-01-08 18:24:00 -0500

    added highscore system

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ window=tk.Tk()
 #initilze score
 global score
 score = 0
-
+global highscore
+with open((Path(__file__).parent / "highscore.txt").resolve(), "r") as f:
+    highscore = f.read()
 
 
 def end():
@@ -27,7 +29,7 @@ def again():
     window.after(50, no.destroy)
     window.after(50,play_again.destroy)
     window.after(50,value6.destroy)
-
+    highscoretext.config(text="Highscore:"+str(highscore))
 
     gameloop()
 
@@ -35,8 +37,7 @@ def again():
 
 entry=tk.Entry(window)
 def gameloop():
-    with open((Path(__file__).parent / "highscore.txt").resolve(), "r") as f:
-        highscore = f.read()
+
 
 
     a= random.randint(0,101)
@@ -55,7 +56,7 @@ def gameloop():
 
     def awnserrecived(event):
 
-        
+     try:
         print(entry.get())
         f=entry.get()
         if 'beans' in f :
@@ -64,10 +65,13 @@ def gameloop():
         window.after(2000,value7.destroy)
         if f == z:
          global score
+         global highscore
          f = int(entry.get())
          print ("correct")
          score= score + 1
          scoreLabel.config(text="Score:"+str(score))
+         if score > int(highscore):
+             highscoretext.config(text="HighsScore:"+ str(score))
          print(score)
          value5.pack()
          window.after(2000, value5.destroy)
@@ -82,6 +86,12 @@ def gameloop():
 
          global value6
          value6 = tk.Label(text="Game Over")
+         if score > int(highscore):
+             highscore = score
+             highscoretext.config(text="new highscore:" + str(score))
+             with open("highscore.txt", "w") as f:
+                 f.write(str(highscore))
+
          global play_again
          play_again = tk.Label(text="Would You Like To Play Again?")
          f = int(entry.get())
@@ -99,6 +109,11 @@ def gameloop():
          yes = tk.Button(text="Yes", command=again)
          no.pack()
          yes.pack()
+     except ValueError:
+         notint=tk.Label(text="that's not a number please enter a number.")
+         entry.delete(first=0,last=10)
+         notint.pack()
+         window.after(1000,notint.destroy)
 
 
 
@@ -112,9 +127,11 @@ def gameloop():
     value1.pack()
     value2.pack()
     value3.pack()
+
 scoreLabel = tk.Label(text="Score:" + str(score))
+highscoretext = tk.Label(text="HighScore:" + str(highscore))
 
-
+highscoretext.pack(side=tk.BOTTOM)
 scoreLabel.pack(side=tk.BOTTOM)
 entry.pack()
 window.after(1000,gameloop)
