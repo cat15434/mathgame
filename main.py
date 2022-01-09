00a4eba4aae4941c93db15b09231ceef5520d0d7
user <cat15434@gmail.com>
2022-01-08 20:30:57 -0500

    made gui nicer added correct awnser when wrong awnser took out correct awnser always shown

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 window=tk.Tk()
-
+window.configure(bg='light blue')
 
 
 
@@ -25,16 +25,19 @@ def end():
 
 #Make Play again button event for yes response
 def again():
+    global correctawnser
     global score
     entry.config(state="normal")
     score=0
-    scoreLabel.config(text="Score:" + str(score))
+    scoreLabel.config(text="Score:" + str(score),bg="light blue")
     window.after(50, yes.destroy)
     window.after(50, no.destroy)
     window.after(50,play_again.destroy)
     window.after(50,value6.destroy)
+    window.after(50,correctawnser.destroy)
     highscoretext.config(text="Highscore:"+str(highscore))
     entry.delete(first=0, last=10)
+
     gameloop()
 
 
@@ -74,7 +77,7 @@ def gameloop():
 #initiate if statement for beans easter egg
         if 'beans' in f :
          print("mmmm beans")
-         value7.pack()
+         value7.grid(row=0,column=2)
 #destroy the .pack for mmmm beans or value7
         window.after(2000,value7.destroy)
 #if the user input is a valid respons and isnt beans but an int then this is the correct awnser response
@@ -94,12 +97,12 @@ def gameloop():
              highscoretext.config(text="HighsScore:"+ str(score))
          print(score)
 #place correct label or value5.pack
-         value5.pack()
+         value5.grid(row=1,column=2)
 #deletes all labels as needed
          window.after(2000, value5.destroy)
-         window.after(100,value2.destroy)
-         window.after(100, value1.destroy)
-         window.after(100, value3.destroy)
+         window.after(1,value2.destroy)
+         window.after(1, value1.destroy)
+         window.after(1, value3.destroy)
 #clears entery to blank
          entry.delete(first=0,last=10)
 
@@ -128,26 +131,29 @@ def gameloop():
          window.after(50, value1.destroy)
          window.after(50,value2.destroy)
          window.after(50,value3.destroy)
-         value6.pack()
+         value6.grid(row=1,column=2)
          entry.delete(first=0,last=10)
+         global correctawnser
+         correctawnser=tk.Label(text="The Correct Answer was: " + z)
+         correctawnser.grid(row=5,column=2)
          entry.config(state="disabled")
          entry.unbind('<Return>')
 #print the play again text
-         play_again.pack()
+         play_again.grid(row=2,column=2)
 #import no button response
          global no
-         no = tk.Button(text="No", command=end)
+         no = tk.Button(text="No", command=end,height=5,width=10)
 #import yes button response
          global yes
-         yes = tk.Button(text="Yes", command=again)
+         yes = tk.Button(text="Yes", command=again,height=5,width=10)
 #show buttons on window
-         no.pack()
-         yes.pack()
+         no.grid(row=3,column=2)
+         yes.grid(row=4,column=2)
 #this is for when user input is not an int but a alphabetical response
      except ValueError:
          notint=tk.Label(text="that's not a number please enter a number.")
          entry.delete(first=0,last=10)
-         notint.pack()
+         notint.grid(row=2,column=2)
          window.after(1000,notint.destroy)
 
 
@@ -160,17 +166,35 @@ def gameloop():
 #makes the submit mechanic for user response
     entry.bind("<Return>",awnserrecived)
 #show the rng numbers and the awnser with .packs
-    value1.pack()
-    value2.pack()
-    value3.pack()
+
+    value1.grid(row=2,column=3)
+    value2.grid(row=2,column=1)
+
+    value1.config(font=("Arial",50),bg="light blue")
+    value2.config(font=("Arial",50),bg="light blue")
+timessign=tk.Label(text="x")
+timessign.config(font=("Arial",50))
+
 #create score & highscore labels
+
 scoreLabel = tk.Label(text="Score:" + str(score))
 highscoretext = tk.Label(text="HighScore:" + str(highscore))
 #make the packs visable as needed
-highscoretext.pack(side=tk.BOTTOM)
-scoreLabel.pack(side=tk.BOTTOM)
-entry.pack()
+highscoretext.grid(row=5,column=2)
+scoreLabel.grid(row=4,column=2)
+scoreLabel.config(font=("Arial",35),bg="light blue")
+highscoretext.config(font=("Arial",30),bg="light blue")
+entry.grid(row=0,column=2)
+timessign.grid(row=2,column=2)
+timessign.config(bg="light blue")
+howto=tk.Label(text="enter your awnser for the product of the 2 numbers above in the entry box")
+howto.grid(row=7,column=2)
+howto.config(font=("Arial",15),bg="light blue")
 #this allows us to create a gameloop inside the window loop of tkinter
+
+
+
 window.after(1000,gameloop)
+
 #run windowloop from tkinter
 window.mainloop()
